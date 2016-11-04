ec96a14a4d343590ff0b3e9d923f87c61eee22f4
Josiah Spence <spencejosiah@gmail.com>
2016-11-04 00:40:06 -0500

    Update Styleguide Fonts display

--- styleguide/examples/atoms-fonts.php
+++ styleguide/examples/atoms-fonts.php
@@ -1,2 +1,2 @@
-<p class="primary-font-demo"><strong>Primary Font:</strong></p>
-<p class="secondary-font-demo"><strong>Secondary Font:</strong></p>
+<p class="primary-font-demo"><strong>Primary Font: Source Sans Pro</strong></p>
+<p class="secondary-font-demo"><strong>Secondary Font: Times New Roman</strong></p>
